9d1a7d36f7d2adc5a8c47b925d8f8147358116b8
a-burba <a.burba@wustl.edu>
2025-11-12 14:36:41 -0600

    getting new course id from displayed description

--- python_web_scraper/web_scraper.py
+++ python_web_scraper/web_scraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 # constants to look for in html (in case they get updated later)
 # note: "class" here refers to html class, I'm using "course" to refer to the thing students can enroll in
 course_wrapper_class = "scpi__classes--row"
+new_course_id_class = "scpi-class__heading middle" # ex: "CSE 3601" is the new version of "CSE 361"
 
 # open html file (I downloaded the page for easier testing, will change later)
 with open("creative_project/python_web_scraper/SP26_11.12.html", "r", encoding="utf-8") as f:
@@ -30,9 +31,16 @@ def parse_course_id(course_id):
 # print data-course-id for each course (see annotated_361.html for example))
 print("code is running!")
 for course in course_wrappers:
+    # get old course id from outer html
     html_wrapper_course_id = course.get('data-course-id')
     formatted = parse_course_id(html_wrapper_course_id)
     if formatted:
-        print(formatted)
+        print(f"Old course id: {formatted}")
     else:
-        print(f"Doesn't match course format: {html_wrapper_course_id}")
+        print(f"Old course id (doesn't match format): {html_wrapper_course_id}")
+        
+    # get new course id from inner html
+    new_course_id_div = course.find('div', class_ = new_course_id_class)
+    if new_course_id_div:
+        print(f"    New course id: {new_course_id_div.get_text(strip=True)}")
+    print()
